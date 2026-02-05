5226584d12a48077d3aee727d1f27055dc1dfa9c
Akshat Shyam <akshatshyam1126@gmail.com>
2026-02-05 14:43:13 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 122333
+let accountEmail = "akki@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Dhanbad"
+
+accountEmail = "a@gmail.com"
+// accountId = "1234321"
+accountCity = "Bokaro"
+accountPassword = "21324354"
+
+
+// accountId = 2
+// console.log(accountId);
+// console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
